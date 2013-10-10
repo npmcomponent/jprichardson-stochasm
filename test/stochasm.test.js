f8eb71692f6d381177e0175e7861308173a98207
JP Richardson <jprichardson@gmail.com>
2013-10-10 09:21:58 -0500

    added test files

---
+++ test/stochasm.test.js
@@ -0,0 +1,10 @@
+var should = require('should')
+  , stochasm = require('package')
+
+describe('+ stochasm', function() {
+  describe('> when no parameters', function() {
+    it('should create a function that generates values between 0.0 and 1.0', function() {
+      
+    })
+  })
+})
