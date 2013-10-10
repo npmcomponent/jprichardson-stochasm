fef6944ad5461b7a2177c2d6a0dbdd3f6c3a9855
JP Richardson <jprichardson@gmail.com>
2013-10-10 14:13:14 -0500

    first test implemented, modified code to work directly with node.js

--- test/stochasm.test.js
+++ test/stochasm.test.js
@@ -1,10 +1,24 @@
 var should = require('should')
-  , stochasm = require('package')
+  , stochasm = require('../lib/stochasm')
+  , u = require('./util')
+  , _ = require('lodash')
+  , cs = require('chi-square')
+
+var SAMPLE_SIZE = 1000//00
+var ERR = 0.1
 
 describe('+ stochasm', function() {
   describe('> when no parameters', function() {
     it('should create a function that generates values between 0.0 and 1.0', function() {
-      
+      var generator = stochasm()
+      var vals = []
+      for (var i = 0; i < SAMPLE_SIZE; ++i) {
+        vals.push(generator.next())
+      }
+
+      u.mean(vals).should.be.approximately(0.5, ERR)
+      _(vals).min().should.be.approximately(0, ERR)
+      _(vals).max().should.be.approximately(1, ERR)
     })
   })
 })
---
+++ test/util.js
@@ -0,0 +1,7 @@
+module.exports.mean = function(vals) {
+  return vals.reduce(function(acc, cur) { return acc + cur }) / vals.length
+}
+
+module.exports.a2b = function(array) {
+  var buffer 
+}
